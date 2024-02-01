e77a8fb802c466f6a12d7ab477bd7f6b31b21eef
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-02-01 16:57:22 +0100

    0.50 maj chemins icone png

--- publique/js/camera.js
+++ publique/js/camera.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
     videoElement.style.objectFit = 'cover';
 
     const imagePaths = {
-        'a': '/home/charlemagne/workspace/Green_IA_website/publique/img/icons/Picto_A.png',
-        'b': '/home/charlemagne/workspace/Green_IA_website/publique/img/icons/Picto_B.png',
-        'c': '/home/charlemagne/workspace/Green_IA_website/publique/img/icons/Picto_C.png',
-        'd': '/home/charlemagne/workspace/Green_IA_website/publique/img/icons/Picto_D.png',
-        'e': '/home/charlemagne/workspace/Green_IA_website/publique/img/icons/Picto_E.png'
+        'a': '../img/icons/Picto_A.png',
+        'b': '../img/icons/Picto_B.png',
+        'c': '../img/icons/Picto_C.png',
+        'd': '../img/icons/Picto_D.png',
+        'e': '../img/icons/Picto_E.png'
     };
 
     function initCamera() {
