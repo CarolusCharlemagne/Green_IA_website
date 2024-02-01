fe87fd736fca173f9b5929c8f96613631c0191a8
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-02-01 11:23:14 +0100

    bouton déclenche caméra et scan

--- publique/js/camera.js
+++ publique/js/camera.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const videoElement = document.getElementById('barcode-scanner');
     const textResultElement = document.getElementById('text_result');
-    const startScannerButton = document.getElementById('start-scanner');
+    const startScannerButton = document.getElementById('start_camera'); // Modifié pour correspondre à l'ID du nouveau bouton
     let isScanning = false;
     let stream = null; // Variable pour stocker le flux vidéo
 
     // Redéfinition des méthodes console.log et console.error pour afficher les messages dans l'élément textResultElement
+    console.originalLog = console.log;
+    console.originalError = console.error;
     console.log = function(...messages) {
         console.originalLog(...messages);
         textResultElement.innerText += messages.join(' ') + '\n';
@@ -55,9 +57,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Fonction pour démarrer le scanner avec le flux vidéo déjà initialisé
-    function startScanner() {
+    function startScanner(event) {
+        event.preventDefault(); // Empêche le comportement par défaut du lien
         if (!stream) {
             console.error('La caméra n\'est pas initialisée.');
+            return;
+        }
+
+        if (isScanning) {
+            console.log('Le scanner est déjà en cours.');
             return;
         }
 
@@ -84,48 +92,49 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
         Quagga.onDetected(function(barcodeScanner) {
-            if (isScanning) return;
-            isScanning = true;
+            if (!isScanning) {
+                isScanning = true;
 
-            const openFoodFactsApiUrl = `https://world.openfoodfacts.org/api/v0/product/${barcodeScanner.codeResult.code}.json`;
+                const openFoodFactsApiUrl = `https://world.openfoodfacts.org/api/v0/product/${barcodeScanner.codeResult.code}.json`;
 
-            fetch(openFoodFactsApiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.status === 0) {
-                        textResultElement.innerText = 'Produit non trouvé';
-                        isScanning = false;
-                        return;
-                    }
-                    let productData = data.product;
-                    let productName = productData.product_name || '';
-                    let brand = productData.brands || '';
-                    let ecoscore = productData.ecoscore_score || '';
-                    let ecoscoreGrade = productData.ecoscore_grade || '';
-                    let displayText = `${productName}\n${brand}\n${ecoscore}\n${ecoscoreGrade}`;
-                    textResultElement.innerText = displayText;
+                fetch(openFoodFactsApiUrl)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.status === 0) {
+                            textResultElement.innerText = 'Produit non trouvé';
+                            isScanning = false;
+                            return;
+                        }
+                        let productData = data.product;
+                        let productName = productData.product_name || '';
+                        let brand = productData.brands || '';
+                        let ecoscore = productData.ecoscore_score || '';
+                        let ecoscoreGrade = productData.ecoscore_grade || '';
+                        let displayText = `${productName}\n${brand}\n${ecoscore}\n${ecoscoreGrade}`;
+                        textResultElement.innerText = displayText;
 
-                    if (productData.image_url) {
-                        let imgElement = document.createElement('img');
-                        imgElement.src = productData.image_url;
-                        imgElement.alt = "Image du produit";
-                        imgElement.style.maxWidth = '100%';
-                        imgElement.style.height = 'auto';
-                        imgElement.style.display = 'block';
-                        imgElement.style.objectFit = 'contain';
+                        if (productData.image_url) {
+                            let imgElement = document.createElement('img');
+                            imgElement.src = productData.image_url;
+                            imgElement.alt = "Image du produit";
+                            imgElement.style.maxWidth = '100%';
+                            imgElement.style.height = 'auto';
+                            imgElement.style.display = 'block';
+                            imgElement.style.objectFit = 'contain';
 
-                        let imgResultElement = document.getElementById('img_result');
-                        imgResultElement.innerHTML = '';
-                        imgResultElement.appendChild(imgElement);
-                    }
-                })
-                .catch(error => {
-                    console.error('Erreur lors de la requête à Open Food Facts:', error);
-                    textResultElement.innerText = 'Erreur lors de la requête à Open Food Facts';
-                })
-                .finally(() => {
-                    setTimeout(() => { isScanning = false; }, 2000);
-                });
+                            let imgResultElement = document.getElementById('img_result');
+                            imgResultElement.innerHTML = '';
+                            imgResultElement.appendChild(imgElement);
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Erreur lors de la requête à Open Food Facts:', error);
+                        textResultElement.innerText = 'Erreur lors de la requête à Open Food Facts';
+                    })
+                    .finally(() => {
+                        setTimeout(() => { isScanning = false; }, 2000);
+                    });
+            }
         });
     }
 
