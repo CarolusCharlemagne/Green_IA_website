9d5a01748657cc3a45d737b2a33a4a43445f9d2f
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-02-22 07:49:19 +0100

    maj taille police et couleur fon au clic boutons

--- functions/clic_but_type.js
+++ functions/clic_but_type.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const boutons = document.querySelectorAll('.bouton_choix_style_cat_dechet');
     const couleurs = [
-        'rgba(183, 229, 205, 0.8)',
-        'rgba(210, 210, 210, 0.8)',
-        'rgba(203, 173, 229, 0.8)',
-        'rgba(210, 180, 140, 0.8)',
-        'rgba(173, 216, 230, 0.8)',
-        'rgba(255, 187, 153, 0.8)',
-        'rgba(255, 255, 153, 0.8)',
-        'rgba(255, 153, 153, 0.8)'
+        'rgba(183, 229, 205, 0.9)',
+        'rgba(210, 210, 210, 0.9)',
+        'rgba(203, 173, 229, 0.9)',
+        'rgba(210, 180, 140, 0.9)',
+        'rgba(173, 216, 230, 0.9)',
+        'rgba(255, 187, 153, 0.9)',
+        'rgba(255, 255, 153, 0.9)',
+        'rgba(255, 153, 153, 0.9)'
     ]; 
     boutons.forEach((bouton, index) => {
         let clicked = false; 
