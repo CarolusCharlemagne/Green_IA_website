2cc6d6d84c616ce92b32a9a8004ee3dd8db4c7ef
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-02-22 18:40:00 +0100

    VERSION STABLE SANS FILTRE

--- functions/depot_gestionaire.js
+++ functions/depot_gestionaire.js
@@ -48,6 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 750);
     });
 
+
     function afficherCarteUtilisateur(latitude, longitude) {
         var carte = L.map('carte_utilisateur').setView([latitude, longitude], 13);
     
@@ -65,18 +66,15 @@ document.addEventListener("DOMContentLoaded", function() {
     function afficherElementsProches(carte, latitudeUtilisateur, longitudeUtilisateur) {
         donneesDechetteries.forEach(function(element) {
             if (distanceEntrePoints(latitudeUtilisateur, longitudeUtilisateur, element.latitude, element.longitude) <= 30) {
-                // Vérifie si au moins un des boutons cliqués correspond à une catégorie avec une valeur de 1 pour l'élément
-                const correspondance = boutonsCliqués.some(boutonCliqué => element[boutonCliqué] === 1);
-                if (correspondance) {
-                    var marqueur = L.marker([element.latitude, element.longitude]).addTo(carte);
-                    marqueur.bindPopup(element.nom);
-                }
+                var marqueur = L.marker([element.latitude, element.longitude]).addTo(carte);
+                marqueur.bindPopup(element.nom);
             }
         });
     }
 
+
     function distanceEntrePoints(lat1, lon1, lat2, lon2) {
-        var R = 6371; // Rayon de la Terre en km
+        var R = 6371; 
         var dLat = (lat2 - lat1) * Math.PI / 180;
         var dLon = (lon2 - lon1) * Math.PI / 180;
         var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
@@ -86,94 +84,6 @@ document.addEventListener("DOMContentLoaded", function() {
         var d = R * c;
         return d;
     }
-
-    // LISTE DES POINTS DE DEPOT
-    const donneesDechetteries = [
-        {
-            nom: "Intermarché",
-            latitude: 43.63241635317403,
-            longitude: 5.138808205954166,
-            ouv_lundi: "08h30",
-            ouv_mardi: "08h30",
-            ouv_mercredi: "08h30",
-            ouv_jeudi: "08h30",
-            ouv_vendredi: "08h30",
-            ouv_samedi: "08h30",
-            ouv_dimanche: "09h00",
-            ferm_lundi: "20h00",
-            ferm_mardi: "20h00",
-            ferm_mercredi: "20h00",
-            ferm_jeudi: "20h00",
-            ferm_vendredi: "20h00",
-            ferm_samedi: "20h00",
-            ferm_dimanche: "13h00",
-            composte: 0,
-            electronique: 0,
-            automobile: 0,
-            carton: 0,
-            papier: 0,
-            verre: 0,
-            piles: 1,
-            ampoules: 1,
-            autre: 0
-        },
-        {
-            nom: "poubelles communes",
-            latitude: 43.63245827171998,
-            longitude: 5.153435340314095,
-            ouv_lundi: "24h",
-            ouv_mardi: "24h",
-            ouv_mercredi: "24h",
-            ouv_jeudi: "24h",
-            ouv_vendredi: "24h",
-            ouv_samedi: "24h",
-            ouv_dimanche: "24h",
-            ferm_lundi: "24h",
-            ferm_mardi: "24h",
-            ferm_mercredi: "24h",
-            ferm_jeudi: "24h",
-            ferm_vendredi: "24h",
-            ferm_samedi: "24h",
-            ferm_dimanche: "24h",
-            composte: 0,
-            electronique: 0,
-            automobile: 0,
-            carton: 1,
-            papier: 1,
-            verre: 0,
-            piles: 0,
-            ampoules: 0,
-            autre: 1
-        },
-        {
-            nom: "Leclerc",
-            latitude: 43.6294124569237,
-            longitude: 5.115618974257699,
-            ouv_lundi: "08h45",
-            ouv_mardi: "08h45",
-            ouv_mercredi: "08h45",
-            ouv_jeudi: "08h45",
-            ouv_vendredi: "08h45",
-            ouv_samedi: "08h45",
-            ouv_dimanche: "08h45",
-            ferm_lundi: "20h00",
-            ferm_mardi: "20h00",
-            ferm_mercredi: "20h00",
-            ferm_jeudi: "20h00",
-            ferm_vendredi: "20h00",
-            ferm_samedi: "20h00",
-            ferm_dimanche: "12h30",
-            composte: 0,
-            electronique: 1,
-            automobile: 0,
-            carton: 0,
-            papier: 0,
-            verre: 0,
-            piles: 1,
-            ampoules: 1,
-            autre: 0
-        }
-    ];
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -187,4 +97,92 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
         console.error("La géolocalisation n'est pas prise en charge par ce navigateur.");
     }
+
+    // LISTE DES POINTS DE DEPOT
+    const donneesDechetteries = [
+    {
+        nom: "Intermarché",
+        latitude: 43.63241635317403,
+        longitude: 5.138808205954166,
+        ouv_lundi: "08h30",
+        ouv_mardi: "08h30",
+        ouv_mercredi: "08h30",
+        ouv_jeudi: "08h30",
+        ouv_vendredi: "08h30",
+        ouv_samedi: "08h30",
+        ouv_dimanche: "09h00",
+        ferm_lundi: "20h00",
+        ferm_mardi: "20h00",
+        ferm_mercredi: "20h00",
+        ferm_jeudi: "20h00",
+        ferm_vendredi: "20h00",
+        ferm_samedi: "20h00",
+        ferm_dimanche: "13h00",
+        composte: 0,
+        electronique: 0,
+        automobile: 0,
+        carton: 0,
+        papier: 0,
+        verre: 0,
+        piles: 1,
+        ampoules: 1,
+        autre: 0
+    },
+    {
+        nom: "poubelles communes",
+        latitude: 43.63245827171998,
+        longitude: 5.153435340314095,
+        ouv_lundi: "24h",
+        ouv_mardi: "24h",
+        ouv_mercredi: "24h",
+        ouv_jeudi: "24h",
+        ouv_vendredi: "24h",
+        ouv_samedi: "24h",
+        ouv_dimanche: "24h",
+        ferm_lundi: "24h",
+        ferm_mardi: "24h",
+        ferm_mercredi: "24h",
+        ferm_jeudi: "24h",
+        ferm_vendredi: "24h",
+        ferm_samedi: "24h",
+        ferm_dimanche: "24h",
+        composte: 0,
+        electronique: 0,
+        automobile: 0,
+        carton: 1,
+        papier: 1,
+        verre: 0,
+        piles: 0,
+        ampoules: 0,
+        autre: 1
+    },
+    {
+        nom: "Leclerc",
+        latitude: 43.6294124569237,
+        longitude: 5.115618974257699,
+        ouv_lundi: "08h45",
+        ouv_mardi: "08h45",
+        ouv_mercredi: "08h45",
+        ouv_jeudi: "08h45",
+        ouv_vendredi: "08h45",
+        ouv_samedi: "08h45",
+        ouv_dimanche: "08h45",
+        ferm_lundi: "20h00",
+        ferm_mardi: "20h00",
+        ferm_mercredi: "20h00",
+        ferm_jeudi: "20h00",
+        ferm_vendredi: "20h00",
+        ferm_samedi: "20h00",
+        ferm_dimanche: "12h30",
+        composte: 0,
+        electronique: 1,
+        automobile: 0,
+        carton: 0,
+        papier: 0,
+        verre: 0,
+        piles: 1,
+        ampoules: 1,
+        autre: 0
+    }
+];
 });
