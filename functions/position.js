582f66d66f674e210a91227208c5431860616356
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-02-22 10:29:25 +0100

    bouton retourne position usr en console

--- functions/position.js
+++ functions/position.js
@@ -2,14 +2,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const boutonPosition = document.getElementById("usr_position");
 
     boutonPosition.addEventListener("click", function() {
-        boutonPosition.style.backgroundColor = '#ccc'; 
-        boutonPosition.style.boxShadow = '0 2px 4px rgba(0,0,0,0.5)'; 
-        boutonPosition.style.transform = 'translateY(2px)'; 
+        boutonPosition.style.backgroundColor = '#ccc';
+        boutonPosition.style.boxShadow = '0 2px 4px rgba(0,0,0,0.5)';
+        boutonPosition.style.transform = 'translateY(2px)';
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                const userPosition = { latitude, longitude };
+                console.log("Position géographique de l'utilisateur:", userPosition);
+            }, function(error) {
+                console.error("Erreur lors de l'obtention de la position:", error.message);
+            });
+        } else {
+            console.error("La géolocalisation n'est pas prise en charge par ce navigateur.");
+        }
 
         setTimeout(function() {
-            boutonPosition.style.backgroundColor = ''; 
-            boutonPosition.style.boxShadow = ''; 
-            boutonPosition.style.transform = ''; 
+            boutonPosition.style.backgroundColor = '';
+            boutonPosition.style.boxShadow = '';
+            boutonPosition.style.transform = '';
         }, 750);
     });
 });
