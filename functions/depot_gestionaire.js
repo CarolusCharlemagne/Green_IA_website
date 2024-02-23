e92b3b88b4b2035deac3d25b54e65256524b2bf0
CarolusCharlemagne <charlemagnecarolus@gmail.com>
2024-02-23 15:48:48 +0100

    affcihae des services dispos 350 px carte

--- functions/depot_gestionaire.js
+++ functions/depot_gestionaire.js
@@ -168,7 +168,7 @@ document.addEventListener("DOMContentLoaded", function() {
     
             let servicesDisponibles = Object.entries(enseigne).reduce((acc, [cle, valeur]) => {
                 if (valeur === 1 && ['composte', 'electronique', 'automobile', 'carton', 'papier', 'verre', 'piles', 'ampoules', 'autre'].includes(cle)) {
-                    return acc + `${cle.charAt(0).toUpperCase() + cle.slice(1)}<br>`;
+                    return acc + `${cle.charAt(0).toUpperCase() + cle.slice(1)},`;
                 }
                 return acc;
             }, "");
